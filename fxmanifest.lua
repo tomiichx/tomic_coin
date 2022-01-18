609ba6dd8c63489c69d39c86eb9e70bdaabad422
Tomić <70343669+tomiichx@users.noreply.github.com>
2022-01-18 22:13:16 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+-- Made by @Tomić#9076
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author "tomiichx"
+description "tCoin / Random Coin PickUP System"
+
+version 'Tomić Development | v1.1'
+
+shared_script '@es_extended/imports.lua'
+
+client_scripts { 
+  'cl_strana.lua'
+}
+
+server_scripts { 
+  '@async/async.lua',
+  '@mysql-async/lib/MySQL.lua',
+  'sv_strana.lua',
+}
+
+shared_script 'config.lua'
+
+--[[
+████████╗░█████╗░███╗░░░███╗██╗░█████╗░
+╚══██╔══╝██╔══██╗████╗░████║██║██╔══██╗
+░░░██║░░░██║░░██║██╔████╔██║██║██║░░╚═╝
+░░░██║░░░██║░░██║██║╚██╔╝██║██║██║░░██╗
+░░░██║░░░╚█████╔╝██║░╚═╝░██║██║╚█████╔╝
+░░░╚═╝░░░░╚════╝░╚═╝░░░░░╚═╝╚═╝░╚════╝░
+--]]
